d0dbb2cc866425c5a608bb48080563fe9203cffb
DIATTA Younousse <92272316+diattakunda@users.noreply.github.com>
2022-02-11 22:35:55 +0000

    entrevoisins derniéres modifications

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/service/DummyNeighbourApiService.java
@@ -0,0 +1,61 @@
+package com.openclassrooms.entrevoisins.service;
+
+import com.openclassrooms.entrevoisins.model.Neighbour;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Dummy mock for the Api
+ */
+public class  DummyNeighbourApiService implements  NeighbourApiService {
+
+    private List<Neighbour> neighbours = DummyNeighbourGenerator.generateNeighbours();
+    private List<Neighbour> favoritesNeighbours = new ArrayList<>();
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Neighbour> getNeighbours() {
+        return neighbours;
+    }
+
+    @Override
+    public List<Neighbour> getFavoritesNeighbours() {
+        return favoritesNeighbours;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void deleteNeighbour(Neighbour neighbour) {
+        neighbours.remove(neighbour);
+    }
+
+    @Override
+    public void deleteFavoritesNeighbours(Neighbour neighbour) {
+        favoritesNeighbours.remove(neighbour);
+    }
+
+    @Override
+    public Neighbour getPositionNeighbour(int position) {
+        return neighbours.get(position);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @param neighbour
+     */
+    @Override
+    public void createNeighbour(Neighbour neighbour) {
+        neighbours.add(neighbour);
+    }
+
+    @Override
+    public void createFavoritesNeighbours(Neighbour neighbour) {
+        favoritesNeighbours.add(neighbour);
+
+    }
+}
